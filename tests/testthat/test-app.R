692eee4f43e7d53e314c4b1667a850c438e6e254
BF <bobby@happycabbage.io>
2023-12-04 04:38:32 -0800

    Updated unit tests, app snapshots, and redoc package

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -3,53 +3,85 @@ library(shinytest2)
 test_that("Initial Shiny values are consistent", {
   app <- AppDriver$new(
     app_dir = "app",
+    shiny_args = list(
+      test.mode = TRUE
+    ),
     name = "app",
     seed = 1000,
     height = 1200,
     width = 1900,
     timeout = 10000,
-    load_timeout = 5000,
+    load_timeout = 15000,
     screenshot_args = FALSE,
     expect_values_screenshot_args = FALSE
   )
+
+  #-----------------------------------------------------
+  # Expect a consistent state on app start
+  #
   app$expect_values()
 
   #-----------------------------------------------------
-  # Set primary selection inputs and custom model inputs
-  # and then Run and expect outputs
+  # Check stats shown on the ui BEFORE filtering
+  #
+  x <- app$get_text(selector = "#stat_skus.stati-value.shiny-bound-input")
+  expect_equal(x, "227,059")
+
+  x <- app$get_text(selector = "#stat_brands.stati-value.shiny-bound-input")
+  expect_equal(x, "10,434")
+
+  x <- app$get_text(selector = "#stat_sales.stati-value.shiny-bound-input")
+  expect_equal(x, "$308,222,837")
+
+  x <- app$get_text(selector = "#stat_units.stati-value.shiny-bound-input")
+  expect_equal(x, "13,393,465")
+
+  #-----------------------------------------------------
+  # Set primary selection inputs to filter sku data
   #
   app$set_inputs(`filters-org` = "ORG001")
-  app$wait_for_idle(500)
+  app$wait_for_idle()
 
   app$set_inputs(`filters-store` = "AA001")
-  app$wait_for_idle(500)
+  app$wait_for_idle()
 
   app$set_inputs(`filters-category3` = "Edibles")
-  app$wait_for_idle(500)
+  app$wait_for_idle(3000)
 
-  app$expect_values(output = c(
-    "stat_skus",
-    "stat_brands",
-    "stat_sales",
-    "stat_units"
-  ))
+  #-----------------------------------------------------
+  # Check stats shown on the ui AFTER filtering
+  #
+  x <- app$get_text(selector = "#stat_skus.stati-value.shiny-bound-input")
+  expect_equal(x, "15")
 
+  x <- app$get_text(selector = "#stat_brands.stati-value.shiny-bound-input")
+  expect_equal(x, "3")
+
+  x <- app$get_text(selector = "#stat_sales.stati-value.shiny-bound-input")
+  expect_equal(x, "$15,543")
+
+  x <- app$get_text(selector = "#stat_units.stati-value.shiny-bound-input")
+  expect_equal(x, "1,014")
+
+
+  #-----------------------------------------------------
+  # Set custom model parameters and run
+  #
   app$set_inputs(sli_trend_pval_conf = c(15, 60))
   app$set_inputs(sli_stock_pval_conf = c(10, 80))
   app$set_inputs(sli_secd_prim = c(20, 50))
   app$set_inputs(sli_npom_ltmi = c(21, 150))
 
   app$click("btn_run")
-  app$wait_for_idle(500)
   app$expect_values()
 
   #-----------------------------------------------------
   # Create report and expect download
   #
   app$click("btn_post")
-  app$wait_for_idle(500)
+  app$wait_for_idle()
   app$set_inputs(dl_format = "html", wait_ = FALSE)
-  app$wait_for_idle(500)
+  app$wait_for_idle()
   app$expect_download("btn_dl")
 
   #-----------------------------------------------------
